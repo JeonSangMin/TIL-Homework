0e02056ebcb164c274a2c6dee13d520eace8fb5f
전상민 <jeonsangmin77@gmail.com>
2019-12-04 00:19:06 +0900

    feat: 12월 3일 과제

---
+++ 과제/Homework_12.03/Homework_12.03/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Homework_12.03
+//
+//  Created by MyMac on 2019/12/03.
+//  Copyright © 2019 sandMan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 과제/Homework_12.03/Homework_12.03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework_12.03
+//
+//  Created by MyMac on 2019/12/03.
+//  Copyright © 2019 sandMan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
